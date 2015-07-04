33240a957d9b4eacfca0238dd07a3234601ccaf1
Wender Freese <wender.jean@gmail.com>
2015-07-04 16:45:54 -0300

    Adding browser-sync support and improve layout

--- app/app/wastt.js
+++ app/app/wastt.js
@@ -1,5 +1,8 @@
 require('angular');
+require('angular-route');
+
 $ = jQuery = require('../bower_components/jquery/dist/jquery.min.js');
 require('../bower_components/bootstrap/dist/js/bootstrap.min.js');
 require('./vendor/controllers')();
+require('./vendor/routes')();
 require('./vendor/application')();
