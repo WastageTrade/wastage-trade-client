d69220b82571efc1c8270227e66d9d69efbc5ca8
Wender Freese <wender.jean@gmail.com>
2015-07-04 15:37:46 -0300

    Revert "Adding browser-sync support and improve layout"

--- app/app/vendor/application.js
+++ app/app/vendor/application.js
@@ -1,7 +1,6 @@
 module.exports = function () {
   var app = angular.module('app', [
     'app.controllers',
-    'app.routes'
   ]);
 
   angular.element(document).ready(function () {
--- app/app/wastt.js
+++ app/app/wastt.js
@@ -1,8 +1,5 @@
 require('angular');
-require('angular-route');
-
 $ = jQuery = require('../bower_components/jquery/dist/jquery.min.js');
 require('../bower_components/bootstrap/dist/js/bootstrap.min.js');
 require('./vendor/controllers')();
-require('./vendor/routes')();
 require('./vendor/application')();
